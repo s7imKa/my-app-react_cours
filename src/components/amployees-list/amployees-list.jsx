0869baef81fb5dfd86ac54e96bc7fa81bcecd40f
s7imKa <maksikmaksik2005@gmail.com>
2025-07-20 01:19:14 +0300

    Refactor AmployeesList component for improved readability

--- src/components/amployees-list/amployees-list.jsx
+++ src/components/amployees-list/amployees-list.jsx
@@ -3,6 +3,7 @@ import AmployeesListItem from '../amployees-list-item/amployees-list-item';
 import './amployees-list.css'
 
 export default function AmployeesList({data}) {
+	
 	return (
 		<ul className='amployees-list list-group'>
 			{data.map((employees, index) => (
