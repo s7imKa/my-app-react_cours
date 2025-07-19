f926bb88f70f084cf2e30f2ed961cdbd58fb9dd6
Maksym <maksikmaks98@gmail.com>
2025-07-19 21:29:06 +0300

    Removes test and web vitals setup for codebase simplification
    Streamlines the project by deleting unused test and performance reporting files.
    Simplifies the main component export and cleans up boilerplate markup.
    Prepares for a lighter, leaner codebase without default testing or web vitals support.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
 }
 
-export default App;
+
